d692c64b1478dad9ab483068bc0029c73f2e1aa6
Travis Grimmond <travisslack1994@outlook.com>
2017-07-24 11:47:46 +0100

    players tab working on website

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require( 'sinatra/contrib/all' ) if development?
 require_relative('controllers/clubs_controller')
 require_relative('controllers/players_controller')
 
-get '/players' do
-  @players = Player.all()
+get '/' do
   erb( :index )
 end
 
